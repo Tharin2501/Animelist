3673bca2b9ecb41b6159228497e926e41fc2e183
Tharin <tharin2501@hotmail.no>
2021-05-09 23:35:29 +0200

    fix: refactoring tests

--- src/tests/app.test.tsx
+++ src/tests/app.test.tsx
@@ -3,6 +3,8 @@ import "@testing-library/jest-dom"; // to use toBeInTheDocument, normally in a s
 import Card from "../components/card/Card";
 import CardList from "../components/card/CardList";
 
+// How to run single test: yarn test -t 'name of test'
+
 // Stub for the Card component returned by map in CardList // TODO add description + test for description
 const cardStub = {
   mal_id: 101,
@@ -10,32 +12,31 @@ const cardStub = {
   image_url: "Test image url",
 };
 
-// Stubbing api data for props:  https://levelup.gitconnected.com/how-to-write-unit-tests-with-react-testing-library-d9624fd2b707
-// image tests: https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
-describe("Card - Test render Card component with props", () => {
-  test("should render Card with mal_id, title and image_url", () => {
-    render(<Card item={cardStub} />);
-    // screen.debug();
-    expect(screen.getByText("Test title")).toBeTruthy(); // Check if title has some value
+beforeEach(() => {
+  render(<Card item={cardStub} />);
+  render(<CardList />);
+  screen.debug();
+});
+
+describe("CardList and Card - Test searchinput and card data(stub)", () => {
+  // Stubbing api data for props:  https://levelup.gitconnected.com/how-to-write-unit-tests-with-react-testing-library-d9624fd2b707
+  // image tests: https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
+  test("Should render Card with correct stub data", () => {
+    expect(screen.getByText("Test title")).toBeTruthy(); // Check if title prop(heading) has some value, better to use getByRole
     expect(screen.getByRole("img")).toHaveAttribute("src", "Test image url"); // getByRole to get most HTML elements. Check if img src/alt has stub value
     expect(screen.getByRole("img")).toHaveAttribute("alt", "Test title");
   });
-});
-
-describe("CardList - Test input", () => {
   test("Should be empty at startup", () => {
-    render(<CardList />);
-    const input = screen.getByPlaceholderText("Search...") as HTMLInputElement;
+    const input = screen.getByRole("textbox", {
+      name: "add logo",
+    }) as HTMLInputElement; // recommended
     expect(input).toBeTruthy();
     expect(input.value).toBe("");
-    screen.debug();
   });
-  test("Should be able to type in Inputfield", () => {
-    render(<CardList />);
+  test("Should be able to type in input", () => {
     const input = screen.getByPlaceholderText("Search...") as HTMLInputElement;
     expect(input).toBeTruthy();
     fireEvent.change(input, { target: { value: "anything user types" } });
     expect(input.value).toBe("anything user types");
-    screen.debug();
   });
 });
