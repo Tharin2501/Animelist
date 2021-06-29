b8b152fd4675a8f200c4f4577a14334cf5f9c9fc
Tharin <tharin2501@hotmail.no>
2021-06-29 17:25:35 +0200

    fix: custom hook for localstorage of search results

--- src/components/card/CardList.tsx
+++ src/components/card/CardList.tsx
@@ -1,19 +1,18 @@
-import { useState } from "react";
-import Styled from "styled-components";
+import styled from "styled-components";
 import Card from "./Card";
 import { ApiDataType } from "./types/CardType";
 import Search from "../search/Search";
-import { useEffect } from "react";
+import useSemiPersistentState from "../../hooks/useSemiPersustentState";
 
 // API:  https://jikan.docs.apiary.io/#reference
-const ContentContainer = Styled.span`
+const ContentContainer = styled.span`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
 `;
 
 // src: https://travishorn.com/some-ways-to-align-the-last-row-in-a-flexbox-grid-720f365dcb16
-const StyledMockCard = Styled.span`
+const StyledMockCard = styled.span`
   width: 200px;
   padding: 37px; // 16px wrapper + 20px card + 1px border = 37px
   visibility: hidden;
@@ -22,32 +21,30 @@ const StyledMockCard = Styled.span`
 const CardList: React.FunctionComponent<ApiDataType> = ({ data }) => {
   /* Currently unused
   const { search } = window.location;
-  // const query = new URLSearchParams(search).get("");  
+  const query = new URLSearchParams(search).get("");  
   */
 
-  //state needs to be "lifted up" to common parent(cardList) to share state with other components
-  const [searchTerm, setSearchTerm] = useState(
-    localStorage.getItem("searchResult") || ""
+  // State needs to be "lifted up" to common parent(cardList) to share state with other components
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    "searchResult",
+    ""
   );
 
   /*
    Currently unused, might find a use case for this function later
    src: https://stackoverflow.com/questions/19590865/from-an-array-of-objects-extract-value-of-a-property-as-array
-  */
+  
   function GetMalId(input: any, field: any) {
     let output: any[] = [];
     for (var i = 0; i < input.length; ++i) output.push(input[i][field]);
     return output;
   }
+  */
 
   // Props are passed top-> bot, use Callback handler to communicate from CardList <- Search
   const handleOnSearch = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setSearchTerm(event.currentTarget.value); // currentTarget = element that has the event listener(input).
   };
-
-  useEffect(() => {
-    localStorage.setItem("searchResult", searchTerm);
-  }, [searchTerm]);
 
   const filterItems = (query: string) => {
     if (!query) {
