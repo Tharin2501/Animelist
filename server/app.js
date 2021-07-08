276449cd157903fac980b210c475c6b2e3c7fa6e
Tharin <tharin2501@hotmail.no>
2021-07-08 20:17:40 +0200

    fix: workaround for doubleclick to post, render confirm btn

--- server/app.js
+++ server/app.js
@@ -1,63 +1,58 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const fetch = require("node-fetch");
+//const fetch = require("node-fetch");
 
 // middleware, allows us to use req.body
 const bodyParser = require("body-parser");
-// allows cors
 const cors = require("cors");
-//  everywhere else in my application instead of requiring pg directly, I'll require this file
+// everywhere else in my application instead of requiring pg directly, I'll require this file
 const db = require("./db");
 
 // Since CRA is using 3000, we’ll just make the server port 3001.
 const PORT = process.env.REACT_APP_SERVER_PORT || 3001;
 
 // Middlewares
+
+// Allows Cors
 app.use(cors());
-// For parsing application/x-www-form-urlencoded
+//To handle JSON payloads
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // For parsing application/json
 app.use(express.json());
 
-// Docs: https://www.npmjs.com/package/node-fetch
-app.get("/", (req, res) => {
-  fetch("https://api.jikan.moe/v3/anime/21")
-    .then((res) => res.json())
-    .then((data) => res.send(data));
-});
+app.post("/api/insert", (req, res) => {
+  // catch the state variable(name) in our frontend
+  const name = req.body.name;
 
-// TEST CAN BE DELETED
-// db.query: the query method we exported earlier from db/index.js
-app.get("/test", (req, res) => {
-  const sqlInsertTest = "INSERT INTO favorite (anime) VALUES ('test anime20')";
-  db.query(sqlInsertTest, (err, res) => {
-    console.log(err, res);
+  //src: https://stackoverflow.com/questions/43755454/bind-message-supplies-1-parameters-but-prepared-statement-requires-2
+  // insert into id and anime columns of favorites with values (to be defined as variables)
+  db.query(
+    {
+      text: "INSERT INTO favorite (name) VALUES ($1)",
+      values: [name],
+    },
+    (error) => {
+      if (error) {
+        throw error;
+      }
+    }
+  ).then(() => {
+    res.status(201).json({ status: "success", message: "POST Success." });
   });
-  res.send("data inserted, check pgadmin");
 });
 
-app.get("/api/get", (req, res) => {
+// might be useful: https://www.taniarascia.com/node-express-postgresql-heroku/
+
+app.get("/", (req, res) => {
+  res.send("in use: /api/insert and /api/get");
+});
+
+app.get("/api/get", (req, res, next) => {
   const sqlSelect = "SELECT * from favorite";
   db.query(sqlSelect, (err, result) => {
-    console.log(res);
     res.send(result);
-  });
-});
-
-// TODO: 14:45 pedrotech
-// this route should INSERT (the posted data from "add to favorites") to pg.
-// need to write a db.query to insert data (just title) into favorites
-app.post("/api/insert", (req, res) => {
-  // catch the state variable in our frontend
-  const animeName = req.body.animeName;
-
-  // MY FIX: https://stackoverflow.com/questions/43755454/bind-message-supplies-1-parameters-but-prepared-statement-requires-2
-  // insert into id and anime columns of favorites with values (to be defined as variables)
-  const sqlInsert = "INSERT INTO favorite (anime) VALUES ($1)";
-  const data = [animeName];
-  db.query(sqlInsert, data, (err, result) => {
-    console.log(result);
   });
 });
 
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { useCallback } from "react";
 import Styled from "styled-components";
 import { CardType } from "./types/CardType";
-import Axios from "axios";
+import axios from "axios";
 
 const Wrapper = Styled.div`
     max-width: 250px;
@@ -37,28 +36,33 @@ const FlexColumn = Styled.span`
   flex-direction: column;
 `;
 
-// TODO: need to save data.title to a state
 const Card: React.FunctionComponent<CardType> = ({ item }) => {
-  const [animeName, setAnimeName] = useState("");
-  //const [animeId, setAnimeId] = useState(null);
+  const [name, setName] = useState<any>("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchAnimeById = useCallback((id: number) => {
-    fetch(`https://api.jikan.moe/v3/anime/${id}`) // need to pass id after anime/id here -> anime/42938
-      .then((response) => response.json())
-      .then((data) => {
-        //console.log("FETCHED" + data.title);
-        setAnimeName(data.title);
-        // console.log(animeName);
+  const handleGetRequest = async (id: number) => {
+    axios
+      .get(`https://api.jikan.moe/v3/anime/${id}`)
+      .then((response) => {
+        setName(response.data.title);
+        setIsLoading(true);
+      })
+      .catch((error) => {
+        // handle error
+        throw error;
       });
-  }, []);
+  };
 
-  // TODO:  POST to http://localhost:3001/api/insert
-  const handleOnClick = (id: number) => {
-    fetchAnimeById(id);
-    Axios.post("http://localhost:3001/api/insert", {
-      animeName: animeName,
-    });
-    console.log("Post success");
+  const handlePostRequest = () => {
+    const dto = {
+      name: name,
+    };
+    axios
+      .post("http://localhost:3001/api/insert", dto)
+      .then(() => console.log(`POST success -> ${name}`))
+      .catch((error) => {
+        throw error;
+      });
   };
 
   return (
@@ -72,11 +76,17 @@ const Card: React.FunctionComponent<CardType> = ({ item }) => {
               <span>Score: {item.score}</span>
               <span>Start date: {item.start_date}</span>
               {item.end_date ? <span>End date: {item.end_date}</span> : null}
-              <button onClick={() => handleOnClick(item.mal_id!)}>
-                Add to favorites
-              </button>
+              {isLoading ? null : (
+                <button onClick={() => handleGetRequest(item.mal_id!)}>
+                  Add to favorites
+                </button>
+              )}
+              {isLoading ? (
+                <button onClick={() => handlePostRequest()}>
+                  Are you sure?
+                </button>
+              ) : null}
             </FlexColumn>
-            <p>{animeName}</p>
           </li>
         </StyledUnorderedList>
       </StyledCard>
