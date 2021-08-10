5a7f189083670875b3153ebe9f9830415618e9f5
Tharin <tharin2501@hotmail.no>
2021-08-11 01:02:31 +0200

    fix: correct way to storybook with docs and argstype, try to use as plop template

--- src/components/storyexample/Button.tsx
+++ src/components/storyexample/Button.tsx
@@ -1,12 +1,17 @@
-import React, { FC, ReactNode } from "react";
+import { FunctionComponent, ReactNode } from "react";
 
 type ButtonProps = {
   children: ReactNode;
   onClick?: () => void;
+  backgroundColor?: string;
 };
 
-export const Button: FC<ButtonProps> = ({ children, onClick }: ButtonProps) => (
-  <button onClick={onClick} type="button">
+export const Button: FunctionComponent<ButtonProps> = ({
+  children,
+  onClick,
+  backgroundColor,
+}: ButtonProps) => (
+  <button style={{ backgroundColor }} onClick={onClick}>
     {children}
   </button>
 );
