de0863c597ea3d3c8424932c9bcad0bbb7d00c4c
Tharin <tharin2501@hotmail.no>
2021-08-23 16:35:35 +0200

    fix: more tests for search

--- cypress/integration/searchbar.spec.ts
+++ cypress/integration/searchbar.spec.ts
@@ -1,11 +1,19 @@
+/* eslint-disable jest/valid-expect */
 
+/*
+    FAQ - Links
+    https://docs.cypress.io/examples/examples/recipes#Fundamentals 
+    - Don't click <a> links in your tests that navigate outside of your application. 
+    Likely this isn't worth testing anyway. Just verify the href, dont click through
+
+*/
 beforeEach(() => {
     cy.log("should visit localhost:3000 before every tests");
     cy.visit("/");
   });
 
 afterEach(() => {
-    cy.get('[data-cy="searchbar"]').find('input').clear()
+   cy.get('[data-cy="searchbar"]').find('input').clear()
   })
 
 describe('searchbar tests', () => {
@@ -27,4 +35,18 @@ describe('searchbar tests', () => {
         .get('[data-cy="searchbar-datacontainer"]').children().should('not.be.empty')
     })
 
+    //src: https://www.cypress.io/blog/2020/12/10/testing-the-anchor-links/ 
+    it('should limit to 15 valid link items in search suggestions', () => {
+        cy.get('[data-cy=searchbar]').find('input').type(' ')
+        .get('[data-cy=searchbar-datacontainer]').children()
+        .should('have.length', 15).each(a => {
+            expect(a).to.have.attr("href").not.contain("undefined")
+          })
+        })
+
+    it('should be able to scroll the scrollbar top/bottom' , () => {
+        cy.get('[data-cy=searchbar]').find('input').type(' ')
+        .get('[data-cy=searchbar-datacontainer]').children().last().scrollIntoView().should('be.visible')
+        .get('[data-cy=searchbar-datacontainer]').children().first().scrollIntoView().should('be.visible')
+    })
 })
